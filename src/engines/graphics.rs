36adf6e667d2facd9ba45c1353a6a5e3aceebb5b
Tinco Andringa <mail@tinco.nl>
2015-04-21 22:49:44 +0200

    three rotating squares

--- src/engines/graphics.rs
+++ src/engines/graphics.rs
@@ -2,10 +2,18 @@ use piston::window::{ WindowSettings, Size };
 use piston::event::*;
 use sdl2_window::Sdl2Window as Window;
 use opengl_graphics::{ GlGraphics, OpenGL };
+use graphics as PistonGraphics;
+
+struct BanaanRectangle {
+	color: [f32; 4],
+	shape: [f64; 4],
+	position: [f64; 2],
+	rotation: f64
+}
 
 pub struct Graphics {
 	gl: GlGraphics, // OpenGL drawing backend.
-	rotation: f64   // Rotation for the square.
+	rectangles: Vec<BanaanRectangle>
 }
 
 impl Graphics {
@@ -13,37 +21,32 @@ impl Graphics {
 		use graphics::*;
 
 		const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-		const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-		const BLUE:  [f32; 4] = [0.0, 0.0, 1.0, 1.0];
-
-		let square = rectangle::square(0.0, 0.0, 50.0);
-		let rotation = self.rotation;
+		
 		let (x, y) = ((args.width / 2) as f64, (args.height / 2) as f64);
-
+		let rectangles = &self.rectangles;
+		
 		self.gl.draw(args.viewport(), |c, gl| {
 			// Clear the screen.
 			clear(GREEN, gl);
 
-			let transform = c.transform.trans(x, y)
-									   .rot_rad(rotation)
-									   .trans(-25.0, -25.0);
+			for banaan in rectangles {
+				let banaan_transform = c.transform
+					.trans(x, y)
+					.trans(banaan.position[0], banaan.position[1])
+					.rot_rad(banaan.rotation)
+					.trans(-25.0,-25.0);
 
-			// Draw a box rotating around the middle of the screen.
-			rectangle(RED, square, transform, gl);
-
-			let transform2 = c.transform
-				.trans(x, y)
-				.trans(100.0, 0.0)
-				.rot_rad(rotation)
-				.trans(-25.0, -25.0);
-
-			rectangle(BLUE, square, transform2, gl);
+				rectangle(banaan.color, banaan.shape, banaan_transform, gl);
+			}
+			
 		});
 	}
 
 	fn update(&mut self, args: &UpdateArgs) {
 		// Rotate 2 radians per second.
-		self.rotation += 2.0 * args.dt;
+		for banaan in &mut self.rectangles {
+			banaan.rotation += 2.0 * args.dt;
+		}
 	}
 
 	pub fn new() {
@@ -56,10 +59,33 @@ impl Graphics {
 		// Create an SDL window.
 		let window = Window::new(opengl, window_settings);
 
+		const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+		const BLUE:  [f32; 4] = [0.0, 0.0, 1.0, 1.0];
+		const YELLOW:[f32; 4] = [1.0, 1.0, 0.0, 1.0];
+
 		// Create a new game and run it.
 		let mut graphics = Graphics {
 			gl: GlGraphics::new(opengl),
-			rotation: 0.0
+			rectangles: vec![
+				BanaanRectangle {
+					color: YELLOW,
+					shape: PistonGraphics::rectangle::square(0.0, 0.0, 50.0),
+					position: [-100.0,0.0],
+					rotation: 0.0
+				},
+				BanaanRectangle {
+					color: BLUE,
+					shape: PistonGraphics::rectangle::square(0.0, 0.0, 50.0),
+					position: [0.0,0.0],
+					rotation: 0.0
+				},
+				BanaanRectangle {
+					color: RED,
+					shape: PistonGraphics::rectangle::square(0.0, 0.0, 50.0),
+					position: [100.0,0.0],
+					rotation: 0.0
+				},
+			]
 		};
 
 		for e in window.events() {
